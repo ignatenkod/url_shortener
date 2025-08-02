37f68dcb49e820a6ebd6245f4bf94575eda3d50b
Diana <dvizh1203@gmail.com>
2025-08-02 16:03:57 +0300

    Update base.py

--- src/url_shortener/database/base.py
+++ src/url_shortener/database/base.py
@@ -1,1 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
+class Base(DeclarativeBase):
+    """Базовый класс для всех моделей SQLAlchemy"""
+    pass
+
+Base = declarative_base(cls=Base)
